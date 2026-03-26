cab720aa2c338db407c4240a90c339f61633997e
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2026-03-26 21:14:18 +0100

    [CMDUTILS][NETWORK][LOGOFF][RUNAS][CMD][SYSTEM] Improve imports and headers for some tools

--- base/system/chkdsk/chkdsk.c
+++ base/system/chkdsk/chkdsk.c
@@ -88,7 +88,7 @@ PCHKDSK Chkdsk;
 // Takes the win32 error code and prints the text version.
 //
 //----------------------------------------------------------------------
-static VOID PrintWin32Error(int Message, DWORD ErrorCode)
+static VOID PrintWin32Error(UINT Message, DWORD ErrorCode)
 {
     ConResPuts(StdErr, Message);
     ConMsgPuts(StdErr, FORMAT_MESSAGE_FROM_SYSTEM,
